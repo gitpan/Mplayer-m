6366219149f4ff40e21666fd77a7a374199c033c
Dileep Mani <ahora@cpan.org>
2013-09-18 05:43:45 +0000

    Import of AHORA/Mplayer-m-0.03 from CPAN.
    gitpan-cpan-distribution: Mplayer-m
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         AHORA/Mplayer-m-0.03.tar.gz
    gitpan-cpan-author:       AHORA
    gitpan-cpan-maturity:     released

--- lib/Mplayer/m.pm
+++ lib/Mplayer/m.pm
@@ -3,7 +3,7 @@ package Mplayer::m;
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw(play);
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 #absolutely essential modules
 use strict;
@@ -14,120 +14,162 @@ use File::Find;
 use List::MoreUtils qw(uniq);
 use File::Basename;
 
-my (@m,$m,@allfolder,@folder,$USER,@allformat,@format,$format,$folder,@match,@allmatch,$match,$style,$i,$choice,@allchoice,$allargv,$allchoice,$directory,$shuffle,@check,$check,@data);
+my (@m,$m,@allfolder,@folder,$USER,@allformat,@format,$format,$folder,@match,@allmatch,$match,$style,$i,$choice,@allchoice,$allargv,$allchoice,$directory,$shuffle,@check,$check,@data,@both);
 
 #getting user name
 $USER=$ENV{USER};
 sub play {
-  
-  #erasing .list file and reading settings file .m
-  if(-f "/home/$USER/.m"){
-    write_file("/home/$USER/.list");
-    @m=read_file("/home/$USER/.m");
-  }else{
-    #for creating config file .m in $ENV{HOME}/
-    @data="#add all paths to your folders after 'FOLDER:' separated by space\nFOLDER:\n\n#all the formats to be played after 'FORMAT:' separated by space\n#popular audio formats include 'mp3 wav flac wma ra rm ram ogg mid'\n#popular video formats include 'mov avi divx mpeg mpg m4p flv wmv'\nFORMAT:mp3 wav flac\n\n#put 'off' to disable shuffle after 'SHUFFLE:'\nSHUFFLE:on";
-    write_file("/home/$USER/.m",@data);
-  }
-  #processing data from .m file
+    
+    #erasing .list file and reading settings file .m
+    if(-f "/home/$USER/.m"){
+	write_file("/home/$USER/.list");
+	@m=read_file("/home/$USER/.m");
+    }else{
+	#for creating config file .m in $ENV{HOME}/
+	@data="#add all paths to your folders after 'FOLDER:' separated by space\nFOLDER:\n\n#all the formats to be played after 'FORMAT:' separated by space\n#popular audio formats include 'mp3 wav flac wma ra rm ram ogg mid m4a'\n#popular video formats include 'mov avi divx mpeg mpg m4p flv wmv'\nFORMAT:mp3 wav flac wma ra rm ram ogg mid m4a\n\n#put 'off' to disable shuffle after 'SHUFFLE:'\nSHUFFLE:on";
+	write_file("/home/$USER/.m",@data);
+    }
+    #processing data from .m file
   foreach $m(@m){
-    $m =~ s/\s+$//;
-    $m=~ s/\s+/ /;
-    if($m=~/^FOLDER:/){
-      @allfolder=split(/:/,$m);
-      if($allfolder[1]){
-	@folder=split(/ /,$allfolder[1]);
-      }else{
-	carp "Error:no FOLDER: provided in /home/$ENV{USER}/.m" and exit;
+      $m =~ s/\s+$//;
+      $m=~ s/\s+/ /;
+      if($m=~/^FOLDER:/){
+	  @allfolder=split(/:/,$m);
+	  if($allfolder[1]){
+	      @folder=split(/ /,$allfolder[1]);
+	  }else{
+	      carp "Error:no FOLDER: provided in /home/$ENV{USER}/.m" and exit;
+	  }
       }
-    }
-    if($m=~/^FORMAT:/){
-      @allformat=split(/:/,$m);
-      if($allformat[1]){
-	@format=split(/ /,$allformat[1]);
-	foreach $format(@format){
-	  $format="$format\$";
+      if($m=~/^FORMAT:/){
+	  @allformat=split(/:/,$m);
+	  if($allformat[1]){
+	      @format=split(/ /,$allformat[1]);
+	      foreach $format(@format){
+		  $format="$format\$";
+	      }
+	  }else{
+	      carp "Error:no FORMAT: provided in /home/$ENV{USER}/.m" and exit;
+	  }
+	  $format=join('|',@format);
+      }
+      if($m=~/^SHUFFLE:/){
+	  @check=split(/:/,$m);
+	  if($check[1] ne 'off'){
+	      $shuffle='-shuffle';
+	  }
+      }
+  }
+    
+    #to the real program
+    #folders matching single keyword
+    if($ARGV[0] and $ARGV[0] eq '-f'){
+	shift (@ARGV);
+	if(@ARGV==0){
+	    @ARGV=".";
 	}
-      }else{
-	carp "Error:no FORMAT: provided in /home/$ENV{USER}/.m" and exit;
-      }
-      $format=join('|',@format);
+	foreach $folder(@folder){
+	    chomp($folder);
+	    find (\&file,"$folder/");
+	}
+	@match=uniq(@allmatch);
+	if(!@match){print "sorry ..no match found\n";exit}
+	print "\n";
+	foreach $match(@match){
+	    $style=$match;
+	    ++$i;
+	    print "\e[1;35m$i\e[0m $style";
+	}
+	print "\n";
+	$choice=<STDIN>;
+	chomp($choice);
+	if($choice){
+	    @allchoice=split(/ /,$choice);
+	    foreach $allchoice(@allchoice){
+		if($allchoice=~/-/){
+		    @both=split(/-/,$allchoice);
+		    if(!$both[0]){$both[0]=1}
+		    if(!$both[1]){$both[1]=@match}
+		    while($both[1] >= $both[0]){
+			find (\&directory,"$match[$both[0]-1]");
+			$both[0]++;
+		    }
+		}else{
+		    find(\&directory,"$match[$allchoice-1]");
+		}
+	    }
+	}else{
+	    foreach $match(@match){
+		find(\&directory,"$match");
+	    }
+	}
+	exec ("mplayer $shuffle -playlist /home/$USER/.list");
     }
-    if($m=~/^SHUFFLE:/){
-      @check=split(/:/,$m);
-      if($check[1] ne 'off'){
-	$shuffle='-shuffle';
-      }
-    }
-  }
-  
-  #to the real program
-  #folders matching single keyword
-  if($ARGV[0] and $ARGV[0] eq '-f'){
-    shift (@ARGV);
-    foreach $folder(@folder){
-      chomp($folder);
-      find (\&file,"$folder/");
-    }
-    @match=uniq(@allmatch);
-    if(!@match){print "sorry ..no match found\n";exit}
-    print "\n";
-    foreach $match(@match){
-      $style=$match;
-      ++$i;
-      print "\e[1;35m$i\e[0m $style";
-    }
-    print "\n";
-    $choice=<STDIN>;
-    chomp($choice);
-    @allchoice=split(/ /,$choice);
-    foreach $allchoice(@allchoice){
-      find(\&directory,"$match[$allchoice-1]");
-    }
-    exec ("mplayer $shuffle -playlist /home/$USER/.list");
-  }
-  #for folders with multiple keywords
-  elsif($ARGV[0] and $ARGV[0] eq '-fs'){
-    shift (@ARGV);
-    $allargv=join('|',@ARGV);
-    foreach $folder(@folder){
-      chomp($folder);
-      find (\&file_s,"$folder/");
-    }
-    @match=uniq(@allmatch);
-    if(!@match){print "sorry ..no match found\n";exit}
-    print "\n";
-    foreach $match(@match){
-      $style=$match;
-      ++$i;
-      print "\e[1;35m$i\e[0m $style";
-    }
-    print "\n";
-    $choice=<STDIN>;
-    chomp($choice);
-    @allchoice=split(/ /,$choice);
-    foreach $allchoice(@allchoice){
-      find(\&directory,"$match[$allchoice-1]");
-    }
-    exec ("mplayer $shuffle -playlist /home/$USER/.list");
-    #play all songs
-  }elsif(!@ARGV){
-    foreach $folder(@folder){
-      chomp($folder);
-      find (\&all,"$folder/");
-    }
-    exec ("mplayer $shuffle -playlist /home/$USER/.list");
-    #play songs matching keyword
-  }elsif($ARGV[0] and $ARGV[0] eq '-s'){
-    shift (@ARGV);
-    $allargv=join('|',@ARGV);
-    foreach $folder(@folder){
-      find (\&song_s,"$folder/");
-    }
-    exec ("mplayer $shuffle -playlist /home/$USER/.list");
-  }elsif($ARGV[0] and $ARGV[0] eq ('-h' or '--help')){
-    #help option
-print "Usage:
+    #for folders with multiple keywords
+    #for folders with multiple keywords
+    elsif($ARGV[0] and $ARGV[0] eq '-fs'){
+	shift (@ARGV);
+	if(@ARGV==0){
+	    @ARGV=".";
+	}
+	$allargv=join('|',@ARGV);
+	foreach $folder(@folder){
+	    chomp($folder);
+	    find (\&file_s,"$folder/");
+	}
+	@match=uniq(@allmatch);
+	if(!@match){print "sorry ..no match found";exit}
+	print "\n";
+	foreach $match(@match){
+	    $style=$match;
+	    ++$i;
+	    print "\e[1;35m$i\e[0m $style";
+	}
+	print "\n";
+	$choice=<STDIN>;
+	chomp($choice);
+	if($choice){
+	    @allchoice=split(/ /,$choice);
+	    foreach $allchoice(@allchoice){
+		if($allchoice=~/-/){
+		    @both=split(/-/,$allchoice);
+		    if(!$both[0]){$both[0]=1}
+		    if(!$both[1]){$both[1]=@match}
+		    while($both[1] >= $both[0]){
+			find (\&directory,"$match[$both[0]-1]");
+			$both[0]++;
+		    }
+		}else{
+		    find(\&directory,"$match[$allchoice-1]");
+		}
+	    }
+	}else{
+	    foreach $match(@match){
+		find(\&directory,"$match");
+	    }
+	}
+	exec ("mplayer $shuffle -playlist /home/$USER/.list");
+	#play all songs
+    }elsif(!@ARGV){
+	foreach $folder(@folder){
+	    chomp($folder);
+	    find (\&all,"$folder/");
+	}
+	exec ("mplayer $shuffle -playlist /home/$USER/.list");
+	#play songs matching keyword
+    }elsif($ARGV[0] and $ARGV[0] eq '-s'){
+	shift (@ARGV);
+	if(@ARGV==0){
+	    @ARGV=".";
+	}
+	$allargv=join('|',@ARGV);
+	foreach $folder(@folder){
+	    find (\&song_s,"$folder/");
+	}
+	exec ("mplayer $shuffle -playlist /home/$USER/.list");
+    }elsif($ARGV[0] and $ARGV[0] eq ('-h' or '--help')){
+	#help option
+	print "Usage:
       m [options] key word [...]
 
       options:
@@ -136,60 +178,60 @@ print "Usage:
           -fs         search folders matching multiple keywords
           -h          This help message
 ";
-  }else{
-    #play songs matching multiple keywords
-    foreach $folder(@folder){
-      chomp($folder);
-      find (\&song,"$folder/");
+    }else{
+      #play songs matching multiple keywords
+	foreach $folder(@folder){
+	    chomp($folder);
+	    find (\&song,"$folder/");
+	}
+	if(-z "/home/$USER/.list"){print "sorry ..no match found\n";exit}
+	exec ("mplayer $shuffle -playlist /home/$USER/.list");
     }
-    if(-z "/home/$USER/.list"){print "sorry ..no match found\n";exit}
-    exec ("mplayer $shuffle -playlist /home/$USER/.list");
-  }
-  
+    
 }
 
 ######
 
 sub song{
-  if($_=~/@ARGV/i and $_=~/$format/){
-    append_file("/home/$USER/.list","$File::Find::name\n");
-  }
+    if($_=~/@ARGV/i and $_=~/$format/i){
+	append_file("/home/$USER/.list","$File::Find::name\n");
+    }
 }
 
 sub song_s{
-  if($_=~/$allargv/i and $_=~/$format/){
-    append_file("/home/$USER/.list","$File::Find::name\n");
-  }
+    if($_=~/$allargv/i and $_=~/$format/i){
+	append_file("/home/$USER/.list","$File::Find::name\n");
+    }
 }
 
 sub directory{
-  if($_=~/$format/){
-    append_file("/home/$USER/.list","$File::Find::name\n");
-  }
+    if($_=~/$format/i){
+	append_file("/home/$USER/.list","$File::Find::name\n");
+    }
 }
 
 sub all{
-  if($_=~/$format/){
-    append_file("/home/$USER/.list","$File::Find::name\n") if -f;
-  }
+    if($_=~/$format/i){
+	append_file("/home/$USER/.list","$File::Find::name\n") if -f;
+    }
 }
 
 sub file{
-  if($_=~/$format/){
-    $directory=dirname($File::Find::name);
-    if($directory=~/@ARGV/i){
-      push (@allmatch,$directory);
+    if($_=~/$format/i){
+	$directory=dirname($File::Find::name);
+	if($directory=~/@ARGV/i){
+	    push (@allmatch,$directory);
+	}
     }
-  }
 }
 
 sub file_s{
-  if($_=~/$format/){
-    $directory=dirname($File::Find::name);
-    if($directory=~/$allargv/i){
-      push (@allmatch,$directory);
+    if($_=~/$format/i){
+	$directory=dirname($File::Find::name);
+	if($directory=~/$allargv/i){
+	    push (@allmatch,$directory);
+	}
     }
-  }
 }
 
 
@@ -212,7 +254,7 @@ Mplayer::m - seek and play
   m -s key word here
 
   #to list all folders
-  m -f .
+  m -f
 
   #to find and play files from a folder matching a keyword ('key word here' matching folders)
   m -f key word here
@@ -220,9 +262,12 @@ Mplayer::m - seek and play
   #to find and play files matching several keywords('key' or ' word' or 'here' matching folders)
   m -fs key word here
 
+  #help
+  m -h
+
 =head1 DESCRIPTION
 
-Module for finding audio/video files and play with mplayer.
+A cool way for finding audio/video files and play with mplayer.
 
 =head1 METHOD
 
@@ -235,12 +280,29 @@ When using m with folder options ( -f or -fs ) it will show a list of folder wit
    1 /media/Music/Rammstein-Greatest Hits/CD1
    2 /media/Music/Rammstein-Greatest Hits/CD2
    3 /media/Music/Rammstein/some collection
+   4 /home/mani/Downloads/Rammstein-Disography
+   5 /home/mani/Downloads/Rammstein
   
 Now select any number of folder listed by entering corresponding number on the left side seperated by space and then pressing Enter.For example
 
   #1 3
 
-will play files in '/media/Music/Rammstein-Greatest Hits/CD1'  &  '/media/Music/Rammstein/some collection'
+will play files in  '/media/Music/Rammstein-Greatest Hits/CD1'   &   '/media/Music/Rammstein/some collection'
+
+ #1 3-5
+
+will play files corresponding to 1,3,4,5
+
+
+ # 2-
+
+will play files corresponding to 2 and folders after it (i.e 2,3,4 and 5)
+
+ #-3
+
+will play files corresponding to 3 and folders before it (i.e 1,2 and 3)
+
+If no folder number is provided, all files in all folders shown will be played (i.e 1,2,3,4 and 5) 
 
 =head2 Content of '.m' file
 
@@ -248,15 +310,17 @@ will play files in '/media/Music/Rammstein-Greatest Hits/CD1'  &  '/media/Music/
   FOLDER:
 
   #all the formats to be played after 'FORMAT:'
-  #popular audio formats include 'mp3 wav flac wma ra rm ram ogg mid'
+  #popular audio formats include 'mp3 wav flac wma ra rm ram ogg mid m4a'
   #popular video formats include 'mov avi divx mpeg mpg m4p flv wmv'
-  FORMAT:mp3 wav flac
+  FORMAT:mp3 wav flac wma ra rm ram ogg mid m4a
 
   #put 'off' to disable shuffle after 'SHUFFLE:'
   SHUFFLE:on
 
 Make sure you fill 'FOLDER:' option eg. FOLDER:/home/mani/Downloads  /media/Entertainment
-By defualt for 'FORMAT:' option, 'mp3,wav and flac' are included.
+
+By defualt for 'FORMAT:' option, 'mp3 wav flac wma ra rm ram ogg mid m4a' are included.
+
 By default 'SHUFFLE:' is on
 
 =head2 EXPORT
